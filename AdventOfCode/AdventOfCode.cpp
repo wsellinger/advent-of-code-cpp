8d33cee155255131fef90f5d0b17622569006f21
William Sellinger <wsellinger@gmail.com>
2023-11-17 11:04:02 -0500

    directory structure reorganized

--- AdventOfCode/AdventOfCode.cpp
+++ AdventOfCode/AdventOfCode.cpp
@@ -1,4 +1,4 @@
-// AdventOfCodeCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// AdventOfCode.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
